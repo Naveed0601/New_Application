0a79efd28394aea811480e62e94c44bbb7b1fa88
naveed <abdulnaveedmohammed81@gmail.com>
2024-12-19 19:18:20 +0530

    connection error

--- server/index.js
+++ server/index.js
@@ -3,13 +3,9 @@ const testapi = require('./routes/test');
 const cors = require("cors");
 
 const app = express();
-const corsOptions = {
-    origin: 'https://inspiring-lens-442115-p5.de.r.appspot.com',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-  };
-  
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: "https://inspiring-lens-442115-p5.de.r.appspot.com",
+}));
 
 app.use('/', testapi);
 
