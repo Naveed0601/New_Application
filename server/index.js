cc342be9c182cc9014f32efc3d09f5291dcca53c
naveed <abdulnaveedmohammed81@gmail.com>
2024-12-19 20:38:15 +0530

    backend change

--- server/index.js
+++ server/index.js
@@ -7,16 +7,16 @@ const app = express();
 app.use(cors());
 
 // Serve React static files
-console.log('Serving static files from:', path.join(__dirname, '../build'));
+console.log('Serving static files from:', path.join(__dirname, '.../build'));
 
-app.use(express.static(path.join(__dirname, '../build'))); // Adjust path if needed
+app.use(express.static(path.join(__dirname, '.../build'))); // Adjust path if needed
 
 // Backend API routes
 app.use('/api', testapi);
 
 // Fallback to React index.html for any other routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html')); // Adjust path if needed
+    res.sendFile(path.join(__dirname, '.../build', 'index.html')); // Adjust path if needed
 });
 
 const port = 1002;
