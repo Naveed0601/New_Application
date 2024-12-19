bcf59b608bf8b1dd48a03e9425ff81c727837429
naveed <abdulnaveedmohammed81@gmail.com>
2024-12-19 21:16:03 +0530

    solve

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <h1 className="text-3xl font-serif font-bold">
-      Welcome to React Msg, asnfahkbfasknd
-    </h1>
+    <h1 className="text-3xl font-serif font-bold">Welcome to React Msg</h1>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -6,18 +6,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-// Serve React static files
-console.log('Serving static files from:', path.join(__dirname, '.../build'));
-
-app.use(express.static(path.join(__dirname, '.../build'))); // Adjust path if needed
-
 // Backend API routes
 app.use('/api', testapi);
-
-// Fallback to React index.html for any other routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '.../build', 'index.html')); // Adjust path if needed
-});
 
 const port = 1002;
 app.listen(port, () => {
