a2013d12c133c381630b88efc0074da8cdb6b03d
naveed <abdulnaveedmohammed81@gmail.com>
2024-12-20 00:24:55 +0530

    frontend change

--- client/src/component/testingPage.jsx
+++ client/src/component/testingPage.jsx
@@ -5,7 +5,7 @@ const TestingPage = () => {
 
   const testingData = async () => {
     try {
-      const response = await fetch("http://35.200.169.163:1002/api/Hii");
+      const response = await fetch("https://35.200.169.163:1002/api/Hii");
       console.log(response);
       const data = await response.json();
       console.log(data);
